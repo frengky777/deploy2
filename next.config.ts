229f4e94d3fbc1a3d3ef104b7c0d0e48255baf83
root <root@Fkyyy>
2025-05-06 19:37:07 +0700

    Update changes

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,8 @@
 import type { NextConfig } from 'next';
+import bcrypt from 'bcryptjs';
+
+const hash = await bcrypt.hash(password, 10);
+const isMatch = await bcrypt.compare(inputPassword, hashedPassword);
 
 const nextConfig: NextConfig = {
   /* config options here */
